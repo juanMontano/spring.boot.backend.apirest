64b31cada8cbaa7cae2829c87fee0330eefc7a1a
Juan Montaño <j.montano.m2@gmail.com>
2018-11-24 13:49:02 -0400

    Initial commit

---
+++ src/main/java/com/jsoft/spring/boot/backend/apirest/spring/boot/backend/apirest/Application.java
@@ -0,0 +1,12 @@
+package com.jsoft.spring.boot.backend.apirest.spring.boot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
